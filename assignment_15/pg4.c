ca81a941484a509605612c7ad2bbae6a8fd30dee
anuragpise27 <anuragpise2003@gmail.com>
2025-05-22 14:36:58 +0530

    Add files via upload

---
+++ assignment_15/pg4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void Pattern(int iRow,int iCol)
+{
+  int i,j;
+  for(i=1;i<=iRow;i++)
+  {
+    for(j=1;j<=iCol;j++)
+    {
+        if(i==1||j==1||i==iCol||j==iRow)
+        {
+            printf("*\t");
+        }
+        else
+        {
+            printf("$\t");
+        }
+    }
+    printf("\n");
+  }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+    printf("enter rows and columns\n");
+    scanf("%d%d",&iValue1,&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
