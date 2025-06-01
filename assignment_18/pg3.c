8d144024fc9bf0f127d27a22e83423024e406f76
anuragpise27 <anuragpise2003@gmail.com>
2025-06-01 15:42:51 +0530

    Add files via upload

---
+++ assignment_18/pg3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+void Display(int Arr[],int iLength)
+{
+ int i;
+ for(i=0;i<iLength;i++)
+ {
+    if(Arr[i]%5==0 && Arr[i]%2==0)
+    {
+        printf("%d",Arr[i]);
+    }
+ }
+
+}
+int main()
+{
+    int i,iSize;
+    int *p = NULL;
+    printf("enter no.");
+    scanf("%d",iSize);
+    p=(int *)malloc(iSize * sizeof(int));
+    if(p==NULL)
+    {
+        printf("unable to alloacte");
+        return -1;
+    }
+    printf("enter elements");
+    for ( i = 0; i < iSize; i++)
+    {
+        scanf("%d",&p[i]);
+    }
+    Display(p,iSize);
+    free(p);
+    return 0;
+    
+}
